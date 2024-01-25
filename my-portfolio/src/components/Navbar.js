be310b915e4caf005a505814c35b0567cf0cce81
Ricardo-ReyesJr <ricardo.reyesjr@hotmail.com>
2024-01-24 21:05:04 -0600

    aliginig the navbar

--- my-portfolio/src/components/Navbar.js
+++ my-portfolio/src/components/Navbar.js
@@ -4,12 +4,17 @@
 
 const Navbar = () => {
     return(
-        <div className="nav-container flex justify-between px-4">
+        <div className="nav-container w-screen items-center text-center flex justify-between px-4">
             <div className="logo">
-                <a href="/">Ricardo Reyes Jr.</a>
             </div>
-            <div className="resume">
-                <a href="" className="ctb-btn">Resume</a>
+            <div className="nav">
+                <ul className="flex align-text-top gap-8">
+                    <li><a href="/" className="ctb-btn">Home</a></li>
+                    <li><a href="" className="ctb-btn">Skills</a></li>
+                    <li><a href="" className="ctb-btn">Projects</a></li>
+                    <li><a href="" className="ctb-btn">Contact Me</a></li>
+                    <li><a href="" className="ctb-btn">Resume</a></li>
+                </ul>
             </div>
         </div>
     )
